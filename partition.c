724e29ab6475541d89f7cb484e5ebf70ec79e7ef
Gkirat01 <gurkirat.singh0701@gmail.com>
2022-10-11 12:50:40 +0530

    committed

--- partition.c
+++ partition.c
@@ -1,114 +1,65 @@
 //code//
-    #include<stdio.h>
+    
 
-    void printarray(int p[], int n)
-
-    {
-
-        int i;
-
-        for ( i = 0; i < n; i++)
-
-           printf("%d ", p[i]);
-
-        printf("\n");
-
-    }
-
-     
-
-    void partition(int n)
-
-    {
-
-        int p[n]; 
-
-        int k = 0;  
-
-        p[k] = n;  
-
-        int rem_val;
-
-        
-
-        
-
-        while (1)
-
-        {
-
-            
-
-            printarray(p, k+1);
-
-            rem_val = 0;
-
-            while (k >= 0 && p[k] == 1)
-
-            {
-
-                rem_val += p[k];
-
-                k--;
-
-            }
-
-     
-
-            
-
-            if (k < 0)  return;
-
-     
-
-            
-
-            p[k]--;
-
-            rem_val++;
-
-     
-
-     
-
-            
-
-            while (rem_val > p[k])
-
-            {
-
-                p[k+1] = p[k];
-
-                rem_val = rem_val - p[k];
-
-                k++;
-
-            }
-
-     
-
-            // Copy rem_val to next position and increment position
-
-            p[k+1] = rem_val;
-
-            k++;
-
-        }
-
-    }
-
-    int main()
-
-    {
-
-        int num;
-
-        printf("\nEnter a number to perform integer partition: ");
-
-        scanf("%d", &num);
-
-        partition(num);
-
-        return 0;
-
-    }
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A utility function that returns true if there is
+// a subset of arr[] with sum equal to given sum
+bool isSubsetSum(int arr[], int n, int sum)
+{
+    // Base Cases
+    if (sum == 0)
+        return true;
+    if (n == 0 && sum != 0)
+        return false;
+ 
+    // If last element is greater than sum, then
+    // ignore it
+    if (arr[n - 1] > sum)
+        return isSubsetSum(arr, n - 1, sum);
+ 
+    /* else, check if sum can be obtained by any of
+        the following
+        (a) including the last element
+        (b) excluding the last element
+    */
+    return isSubsetSum(arr, n - 1, sum)
+           || isSubsetSum(arr, n - 1, sum - arr[n - 1]);
+}
+ 
+// Returns true if arr[] can be partitioned in two
+// subsets of equal sum, otherwise false
+bool findPartiion(int arr[], int n)
+{
+    // Calculate sum of the elements in array
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+ 
+    // If sum is odd, there cannot be two subsets
+    // with equal sum
+    if (sum % 2 != 0)
+        return false;
+ 
+    // Find if there is subset with sum equal to
+    // half of total sum
+    return isSubsetSum(arr, n, sum / 2);
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = { 3, 1, 5, 9, 12 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+   
+    // Function call
+    if (findPartiion(arr, n) == true)
+        cout << "Can be divided into two subsets "
+                "of equal sum";
+    else
+        cout << "Can not be divided into two subsets"
+                " of equal sum";
+    return 0;
+}
+      
